820febc5b059a61fed9b992adfd7e8dbd3c86e4d
blob#0005 <88004068+blob0005@users.noreply.github.com>
2022-05-24 19:03:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,94 @@
+anyerror = False
+try:
+    import requests
+    import colorama
+except:
+    anyerror = True
+if anyerror == True:
+    autfix = input("Missing Modules, Wanna Try Auto Fix (y/n): ")
+    if autfix == "n":
+        exit()
+    if autfix == "y":
+        try:
+            import os
+            os.system("pip install colorama")
+            os.system("pip install requests")
+            print("Problem May Be Fixed Now, Restart The Program")
+            input("")
+            exit()
+        except:
+            print("Error While Fixing, Press Enter To Exit Program")
+            input("")
+            exit()
+import random, time, threading
+try:
+    import os
+    from os import system
+    system("title " + "Roblox Group Sniper")
+except:
+    pass
+colorama.init(autoreset=True)
+def sniper():
+    while True:
+        groupid = random.randint(1000000, 9000000)
+        groupid = str(groupid)
+        id = str(groupid)
+        r = requests.get("https://www.roblox.com/groups/group.aspx?gid="+str(groupid)).text
+        r = str(r)
+        if "owned" not in r:
+            r2 = requests.get(f"https://groups.roblox.com/v1/groups/"+groupid)
+            if "isLocked" not in r2.text and "owner" in r2.text:
+                entry = r2.json()["publicEntryAllowed"]
+                owner = r2.json()["owner"]
+                if entry == True and owner == None:
+                    print(colorama.Fore.GREEN + "Valid Group! https://roblox.com/groups/"+id)
+                    if save == "y":
+                        validfile = open("valid_groups.txt", "a")
+                        validfile.write("https://roblox.com/groups/"+id+"\n")
+                        validfile.close()
+                    try:
+                        r = requests.post(webhook, json={"content": "@here **Sniper Group** https://roblox.com/groups/"+id})
+                    except:
+                        pass
+            else:
+                print(colorama.Fore.RED + "Invalid Group! https://roblox.com/groups/"+id)
+                if save == "y":
+                    invalidfile = open("invalid_groups.txt", "a")
+                    invalidfile.write("https://roblox.com/groups/"+id+"\n")
+                    invalidfile.close()
+        else:
+            print(colorama.Fore.RED + "Invalid Group! https://roblox.com/groups/"+id)
+            if save == "y":
+                invalidfile = open("invalid_groups.txt", "a")
+                invalidfile.write("https://roblox.com/groups/"+id+"\n")
+                invalidfile.close()
+        time.sleep(float(delay))
+while True:
+    try:
+        threads = input("Enter How Many Threads (If To Many Printing The Codes Will Get Laggy): ")
+        threads = int(threads)
+        break
+    except:
+        print("Enter A Valid Choice")
+while True:
+    try:
+        delay = input("Enter Delay For Each Thread (0 = None): ")
+        delay = float(delay)
+        break
+    except:
+        print("Enter A Valid Chioce")
+webhook = input("Enter Webhook (Leave Blank For None): ")
+while True:
+    save = input("Wanna Auto Save All Groups (y/n): ")
+    if save == "y" or save == "n":
+        break
+    else:
+        print("Enter A Valid Choice")
+try:
+    for u in range(int(threads)):
+        t1 = threading.Thread(target=sniper)
+        t1.start()
+except Exception:
+    print("Unkown Error")
+    input("")
+    exit()
